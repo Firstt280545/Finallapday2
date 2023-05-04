afc1b055b84de11d2ec7ec322b8576971aead98a
FirstFlirt Lomami <FirstFlirt Lomami@DESKTOP-OKMOC4A>
2023-05-04 09:56:50 +0700

    Add project files.

---
+++ Finallapday2/Controllers/EmployeeController.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Database;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+
+        [Route("api/[controller]")]
+        [ApiController]
+        public class EmployeeController : ControllerBase
+        {
+            private readonly DataDbContext _dbContext;
+
+            public EmployeeController(DataDbContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
+
+            //Get Method
+            //-getEmployees : แสดงข้อมูล Employee ทุกคน
+            [HttpGet]
+            public async Task<ActionResult<List<employees>>> getEmployees()
+            {
+                var employees = await _dbContext.employees.ToListAsync();
+
+                if (employees.Count == 0)
+                {
+                    return NotFound();
+
+                }
+
+                return Ok(employees);
+            }
+            //-getEmployeeById : แสดงข้อมูล Employee ที่ตรงกับ Id ที่ระบุมา      
+            [HttpGet("{id}")]
+            public async Task<ActionResult<employees>> getEmployeeById(string id)
+            {
+                var employees = await _dbContext.employees.FindAsync(id);
+
+                if (employees == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(employees);
+            }
+
+
+        //-getEmployeeSalary : แสดงเงินเดือนปัจจุบันของ Employee ที่ระบุผ่าน Id
+        [HttpGet("Current Salary")]
+        public async Task<ActionResult<employees>> getEmployeeSalary(string id, int year)
+        {
+
+            var employee = _dbContext.employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            var position = _dbContext.positions.Find(employee.positionId);
+            if (position == null)
+            {
+                return NotFound();
+            }
+            var salary = (position.baseSalary + (position.baseSalary * position.salaryIncreaseRate)) * (year - 1);
+
+            return Ok(salary);
+        }
+
+        //-calEmpSalaryInYear : แสดงผลการคำนวนเงินเดือนของ Employee ที่ระบุในอนาคตอีก n ปีข้างหน้า
+        [HttpGet("Future Salary")]
+        public async Task<ActionResult<employees>> calEmpSalaryInYear(string id, int year)
+        {
+            var employee = _dbContext.employees.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            var position = _dbContext.positions.Find(employee.positionId);
+            if (position == null)
+            {
+                return NotFound();
+            }
+
+            var currentYear = DateTime.Now.Year;
+            var totalYear = year - 1;
+            var salary = position.baseSalary;
+
+            if (totalYear <= currentYear)
+            {
+                return BadRequest();
+            }
+
+            for (int i = currentYear + 1; i <= totalYear; i++)
+            {
+                salary = (float)(salary * 1.15);
+            }
+
+            return Ok(salary);
+        }
+
+
+
+        //Post Method
+        //-createEmployees : เพิ่มข้อมูลพนักงานใหม่ โดยมีเงื่อนไขคือ positionId ของพนักงานจะต้องมีอยู่ใน Table Positions
+        [HttpPost]
+        public async Task<ActionResult<employees>> createEmployees(employees employees)
+        {
+            try
+            {
+                var position = _dbContext.positions.FirstOrDefault(p => p.positionId == employees.positionId);
+                if (position == null)
+                {
+                    return BadRequest("Invalid position ID");
+                }
+
+                _dbContext.employees.Add(employees);
+
+                _dbContext.SaveChanges();
+            }
+
+            catch (DbUpdateException)
+
+            {
+                return BadRequest();
+            }
+            return Ok(employees);
+        }
+
+
+        //Put Method
+        //- updateEmployees : อัพเดทข้อมูลพนักงานโดยมีเงื่อนไขคือถ้ามีการอัพเดท positionId จะต้องเป็น position ที่มีอยู่ใน Table positions
+        [HttpPut]
+        public async Task<ActionResult<employees>> putEmployees(string id, employees newEmployees)
+        {
+            try
+            {
+                if (_dbContext.positions.FirstOrDefault(p => p.positionId == newEmployees.positionId) == null)
+                {
+                    return BadRequest("Invalid position ID");
+                }
+                var employees = await _dbContext.employees.FindAsync(id);
+                if (employees == null)
+                {
+                    return NotFound();
+                }
+                employees.empName = newEmployees.empName;
+                employees.Email = newEmployees.Email;
+                employees.phoneNumber = newEmployees.phoneNumber;
+                employees.hireDate = newEmployees.hireDate;
+                employees.positionId = newEmployees.positionId;
+
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest();
+            }
+            return Ok(newEmployees);
+        }
+
+
+        //Delete Method
+        //- deleteEmployees : ลบข้อมูลพนักงาน
+        [HttpDelete]
+            public async Task<ActionResult<employees>> deleteEmployees(int id)
+            {
+                var employees = await _dbContext.employees.FindAsync(id);
+
+                if (employees == null)
+                {
+                    return NotFound();
+                }
+
+                //Remove employees
+                _dbContext.employees.Remove(employees);
+
+                //save
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(employees);
+            }
+
+        }
+    }
+
---
+++ Finallapday2/Controllers/PositionsController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Database;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PositionsController : ControllerBase
+    {
+        private readonly DataDbContext _dbContext;
+
+        public PositionsController(DataDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //Get Method
+        //- getPositions : แสดงข้อมูล Positions ทั้งหมด
+        [HttpGet]
+        public async Task<ActionResult<List<positions>>> getpositions()
+        {
+            var positions = await _dbContext.positions.ToListAsync();
+
+            if (positions.Count == 0)
+            {
+                return NotFound();
+
+            }
+
+            return Ok(positions);
+        }
+
+
+        //- getPositionById : แสดงข้อมูล Positions ที่ตรงกับ Id ที่ระบุมา
+        [HttpGet("{id}")]
+        public async Task<ActionResult<positions>> getPositionById(int id)
+        {
+            var positions = await _dbContext.positions.FindAsync(id);
+
+            if (positions == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(positions);
+        }
+
+        //- getEmpByPositionId : แสดงรายชื่อพนักงานทุกคนที่อยู่ในตำแหน่งที่ระบุ
+        [HttpGet("Positions ID")]
+        public async Task<ActionResult<List<employees>>> getEmpPositionsID(string positionId)
+        {
+            var employees = _dbContext.employees.Where(e => e.positionId == positionId).ToList();
+
+            if (employees.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(employees);
+        }
+
+
+
+        // Post Method
+        //-createPositions : เพิ่มข้อมูลตำแหน่งใหม่
+        [HttpPost]
+        public async Task<ActionResult<positions>> createPositions(positions positions)
+        {
+            try
+            {
+                _dbContext.positions.Add(positions); //Add positions to Database
+                await _dbContext.SaveChangesAsync(); //Save
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest();
+            }
+
+            return Ok(positions);
+        }
+
+        // Put Method
+        //- updatePositions : อัพเดทข้อมูลตำแหน่ง
+        [HttpPut("id")]
+        public async Task<ActionResult<positions>> updatePositions(string id, positions newpositions)
+        {
+            var positions = await _dbContext.positions.FindAsync(id);
+            if (positions == null)
+            {
+                return NotFound();
+            }
+
+            positions.positionId = newpositions.positionId;
+            positions.positionName = newpositions.positionName;
+            positions.baseSalary = newpositions.baseSalary;
+            positions.salaryIncreaseRate = newpositions.salaryIncreaseRate;
+
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        // Delete Method
+        //- deletePositions : ลบข้อมูลตำแหน่งโดยห้ามลบถ้าหากยังมีพนักงานที่อยู่ในตำแหน่งนั้น
+        [HttpDelete]
+        public async Task<ActionResult<positions>> deletePositions(string id)
+        {
+            var employees = _dbContext.employees.Where(e => e.positionId == id).ToList();
+            if (employees != null && employees.Count > 0)
+            {
+                return BadRequest("Delete location data, do not delete it if there is still an employee at that location.");
+            }
+            var position = _dbContext.positions.SingleOrDefault(p => p.positionId == id);
+            if (position == null)
+            {
+                return NotFound();
+            }
+            _dbContext.positions.Remove(position);
+            _dbContext.SaveChanges();
+            return Ok();
+
+        }
+    }
+}
